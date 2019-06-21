664290afab31a26fa1d4bd1154796ff3efe4783a
xisyn <xisyndi@gmail.ru>
2019-06-21 20:21:50 +0700

    first point

--- src/test/java/Search.java
+++ src/test/java/Search.java
@@ -1,10 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Search {
+    ChromeDriver wd;
+
+    @BeforeMethod
+    public void setUp() {
+        wd = new ChromeDriver();
+    }
+
     @Test
     public void searchTest() {
-        ChromeDriver wd = new ChromeDriver();
         wd.get("https://google.com");
+
+        String fullName = "Хицунов Дмитрий Иванович";
+
+        WebElement searchField = wd.findElement(By.xpath("//div/input[@title=\"Поиск\"]"));
+        searchField.sendKeys(fullName);
+        searchField.submit();
+
+        WebElement firstResult = wd.findElement(By.xpath("(//*[@class=\"st\"]/*)[1]"));
+
+        Assert.assertEquals(firstResult.getText(), fullName);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
     }
 }
