4226bb56fb74da41d9fe63ff497aa5d2b9accc5f
xisyn <xisyndi@gmail.ru>
2019-06-23 14:58:49 +0700

    add test for json schema

--- rest/src/test/java/RestTests.java
+++ rest/src/test/java/RestTests.java
@@ -1,6 +1,13 @@
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -10,12 +17,11 @@ import static org.hamcrest.Matchers.equalTo;
 public class RestTests {
 
     @Test
-    public void test() throws IOException, URISyntaxException {
-
+    public void testMethod() throws IOException, URISyntaxException {
         given()
             .params("key", "AIzaSyA4juEzAjVFoyK64ZVG0D_S-96sR_HsLtg",
                 "q", "7703228474",
-                "cx", "009273001490562717861:hi7m1d4fgss")
+                        "cx", "009273001490562717861:hi7m1d4fgss")
         .when()
             .get("https://www.googleapis.com/customsearch/v1")
         .then()
@@ -24,7 +30,22 @@ public class RestTests {
         .and()
             .contentType(ContentType.JSON)
         .and()
-            .header("Transfer-Encoding", equalTo("chunked"));
+            .header("Transfer-Encoding", equalTo("chunked"))
+        .and()
+            .body("items.title[0]", Matchers.equalTo("ООО \"Танк-к\", Москва (ИНН 7703228474, ОГРН 1037739437966)"));
+    }
+
+    @Test
+    public void testSchema() {
+        given()
+            .params("key", "AIzaSyA4juEzAjVFoyK64ZVG0D_S-96sR_HsLtg",
+                "q", "7703228474",
+                        "cx", "009273001490562717861:hi7m1d4fgss")
+        .when()
+            .get("https://www.googleapis.com/customsearch/v1")
+        .then()
+            .assertThat()
+            .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("jsonBigSchema.json"));
     }
 
 }
